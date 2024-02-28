385139898eef714b654e20ca8632bfa00b7a1686
JustusOmbok <ombokjustus@gmail.com>
2024-02-28 00:49:11 -0800

    optimized solution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,23 +6,30 @@ Module for making change
 
 def makeChange(coins, total):
     """
-    Determines the fewest number of coins
-    needed to meet a given amount total
+    Determines the fewest number of coins needed to meet a given amount total
     """
     if total <= 0:
         return 0
 
-    # Create a list store the for each amount up to total
-    dp = [float('inf')] * (total + 1)
+    # Sort coins in descending order
+    coins.sort(reverse=True)
 
-    # Base case: 0 coins needed to make amount 0
+    # Greedy approach
+    count = 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
+
+    if total == 0:
+        return count
+
+    # Dynamic programming approach
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    # Loop through each coin value
     for coin in coins:
-        # Update dp[i] coin reduces the number of coins needed
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If dp[total] cannot be met by any number of coins
-    return dp[total] if dp[total] != float('inf') else -1
+    return min(count, dp[total]) if dp[total] != float('inf') else -1
