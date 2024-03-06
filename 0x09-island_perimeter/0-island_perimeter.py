ca779e1314c10b8bc0458a9a1cf32c2ebdeb2f10
JustusOmbok <ombokjustus@gmail.com>
2024-03-05 23:06:09 -0800

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Island perimeter module.
+"""
+
 
 def island_perimeter(grid):
+    """Computes perimeter of an island.
+    """
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
@@ -9,14 +14,10 @@ def island_perimeter(grid):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                # Start with the assumption that all sides are perimeter
 
-                # Check adjacent cells (up, down, left, right)
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                    # Deduct 2 for each adjacent land cell (up)
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-                    # Deduct 2 for each adjacent land cell (left)
 
     return perimeter
